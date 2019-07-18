5a2cfd3491370071e49f498d7d28e10a2efd064a
albert <albert@jostle.me>
2019-07-17 21:09:48 -0700

    Added vendor.conf parsing

---
+++ src/extractors/VendorConfExtractor.ts
@@ -0,0 +1,103 @@
+import {Dependency, DependencyManagementFile} from "../../api/deps";
+import Extractor from "./Extractor";
+import ExtractorFile from "./ExtractorFile";
+import parseImportPath from "./goutils/parseImportPath";
+import Languages from "./Languages";
+
+interface ID {
+    organization: string;
+    module: string;
+}
+
+const fileName = "vendor.conf";
+const organizationString = "organization";
+const moduleString = "module";
+
+function parseName(dependency: string): ID {
+    let organization = "_";
+    let module = "";
+    let dependencyArray = [];
+    dependencyArray = dependency.split("/");
+    if (dependency.includes("github.com")) {
+        organization = dependencyArray[1];
+        module = dependencyArray[2];
+    } else if (dependency.includes("golang.org")) {
+        organization = dependencyArray[0];
+        module = dependencyArray[2];
+    } else {
+        organization = dependencyArray[0];
+        module = dependencyArray[1];
+        if (dependencyArray[2] != null) {
+            module = module + "/" + dependencyArray[2];
+        }
+    }
+
+    return { organization, module };
+}
+
+export default class VendorConfExtractor implements Extractor {
+    public requires(): string[] {
+        return [ fileName ];
+    }
+
+    public async extract(files: { [p: string]: ExtractorFile }): Promise<DependencyManagementFile> {
+        const content = files[fileName].raw();
+
+        const lines = content.split(/\n+/g);
+
+        let id = {};
+        id[organizationString] = null;
+        id[moduleString] = null;
+        let idFlag = true;
+        const dependencies = [];
+
+        for (let i = 0; i < lines.length; i++) {
+            let line = lines[i].trim();
+            if (line.length === 0) {
+                continue; // empty line
+            }
+
+            const parts = line.split(/\s+/);
+            const directive = parts[0];
+            if (directive === '#') {
+                continue;
+            }
+            if (idFlag) {
+                id = parseName(directive);
+                idFlag = false;
+                continue;
+            }
+
+            const version = parts[1];
+            var { organization, module } = parseName(directive);
+
+            let scopes = [];
+            if (parts[2] != null) {
+                scopes.push(parts[2]);
+            }
+
+            const dependencyMap: Dependency = {
+                organization: organization,
+                module: module,
+                versionConstraint: version,
+                scopes: scopes,
+            };
+
+            dependencies.push(dependencyMap);
+
+        }
+
+        if (id[organizationString] === null || id[moduleString] === null) {
+            throw new Error(`parse error: no module present`);
+        }
+
+        return {
+            language: Languages.GO,
+            system: "vendor",
+            organization: id[organizationString],
+            module: id[moduleString],
+            version: "latest",
+            dependencies,
+        };
+    }
+}
